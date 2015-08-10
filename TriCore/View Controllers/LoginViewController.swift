3e163e68864a249e3d5f5ba136365002c551ece8
Kyle K Zawacki <z95sk8@outlook.com>
2015-08-09 21:24:08 -0500

    Text input smoothed out on login screen

--- TriCore/View Controllers/LoginViewController.swift
+++ TriCore/View Controllers/LoginViewController.swift
@@ -8,17 +8,77 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
     @IBOutlet weak var tricoreName: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    var keyboardShowing:Bool = false
+    var keyboardHeight:CGFloat = 0
+    var originalCenter:CGPoint?
 
-    override func viewDidLoad() {
+    // MARK: Initialization
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         self.navigationController!.navigationBar.tintColor = UIColor.whiteColor()
         
         self.tricoreName.font = self.tricoreName.font.fontWithSize(self.view.frame.width/5.5)
-        print("Font Size: \(self.view.frame.width/5.5)")
+        
+        self.usernameField.delegate = self
+        self.passwordField.delegate = self
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "textFieldPressed:", name: UIKeyboardWillShowNotification, object: nil)
+        self.originalCenter = self.view.center
     }
     
+    // MARK: User Interaction Events
+    @IBAction func loginButtonPressed(sender: UIButton)
+    {
+        self.usernameField.resignFirstResponder()
+        self.passwordField.resignFirstResponder()
+        
+        self.keyboardShowing = false
+        self.view.center = self.originalCenter!
+    }
+    
+    func textFieldPressed(sender:NSNotification)
+    {
+        print("\(sender.userInfo)")
+        
+        let userInfo = sender.userInfo
+        
+        if let userInfo = userInfo
+        {
+            self.keyboardHeight = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.height
+            
+            if(!self.keyboardShowing)
+            {
+                self.view.center = CGPointMake(self.view.center.x, self.view.center.y - keyboardHeight)
+                self.keyboardShowing = true
+            }
+            
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        self.view.center = self.originalCenter!
+        self.keyboardShowing = false
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // MARK: Login
+    func attemptLogin(withUsername username:String, andPassword password:String)
+    {
+        
+    }
     
 }
